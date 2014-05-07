bf93c15876dd6ccdca0025dfebcad32d2d926d04
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2014-05-07 22:33:26 +0200

    modified:   picostack/templates/instances.html - Looping over formset - Handling no entries - Template now contains a table based view with each row being a set form   fields for corresponding data model.
    modified:   picostack/urls.py
    - Root redirects to a new URL called /instances which lists found instances
      as implemented by manage_instances() view.
    
    modified:   picostack/vms/views.py
    - Working with correct modelformset_factory() to access VmInstance.objects data
      to populate the formset.
    - Started handling the POST arguments as according to the name of the submit
      button.

--- picostack/urls.py
+++ picostack/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'picostack.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'picostack.vms.views.instances', name='home'),
+    url(r'^$', RedirectView.as_view(url='/instances', permanent=False), name='instances'),
+    url(r'^instances/', 'picostack.vms.views.manage_instances', name='instances'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- picostack/vms/views.py
+++ picostack/vms/views.py
@@ -1,27 +1,47 @@
 from django.shortcuts import render
-from django import forms
 from django.http import HttpResponseRedirect
+from django import forms
+from django.forms.models import modelformset_factory, ModelForm
+from picostack.vms.models import VmInstance
 
 
-class InstancesForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField()
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+class VmInstanceForm(ModelForm):
+    class Meta:
+        model = VmInstance
+        fields = ['name', 'current_state', 'image', 'flavour']
+    #current_state = forms.ChoiceField(widget = forms.TextInput(attrs={'readonly':'readonly'}))
 
 
-def instances(request):
+def manage_instances(request):
+    VmInstancesFormSet = modelformset_factory(model=VmInstance,
+                                              form=VmInstanceForm,
+                                              # No empty forms..
+                                              extra=0)
     # If the form has been submitted...
     if request.method == 'POST':
-        # ContactForm was defined in the previous section
-        form = InstancesForm(request.POST)  # A form bound to the POST data
-        if form.is_valid():  # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            return HttpResponseRedirect('/thanks/')  # Redirect after POST
+        # Process the data in form.cleaned_data
+        # ...
+        if '_save' in request.POST:
+            return HttpResponseRedirect('/save/')
+        elif '_start' in request.POST:
+            return HttpResponseRedirect('/start/')
+        elif '_stop' in request.POST:
+            return HttpResponseRedirect('/stop/')
+        elif '_trash' in request.POST:
+            return HttpResponseRedirect('/trash/')
+        return HttpResponseRedirect('/thanks/')  # Redirect after POST
+        # # ContactForm was defined in the previous section
+        # form = VmInstanceForm(request.POST)  # A form bound to the POST data
+        # # if form.is_valid():  # All validation rules pass
+        # return HttpResponseRedirect('/trash/')
     else:
-        form = InstancesForm()  # An unbound form
+        vm_instances_formset = VmInstancesFormSet()
+        columns = list()
+        if vm_instances_formset.total_form_count > 0:
+            columns = [field.label_tag for field
+                       in vm_instances_formset.forms[0].visible_fields()]
 
     return render(request, 'instances.html', {
-        'form': form,
+        'formset': vm_instances_formset,
+        'columns': columns,
     })
